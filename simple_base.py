b12f3f22e673470793ed951e6d55d3c5d1600102
Pietro Porsch Wilhelms <97356900+Hanabe-Fascart@users.noreply.github.com>
2024-07-22 12:33:12 -0300

    Adding basic information

--- simple_base.py
+++ simple_base.py
@@ -1,14 +1,29 @@
-//base imports to be used when creating the Macros module
+#base imports to be used when creating the Macros module
 from pyautogui import *
-import pyautogui          // To locate pixels in the screen
-import time               // To be able to give delay between actions
-import keyboard           // To be able to Use the keyboard actions
-import random             // To give random values, give a more human apearance for certain actions that requiere
-import win32api, win32con // To be able to use the Mouse events, a windows API, faster than the Pythion API
-// Could use pyautogui to click, but as said, Windows API is WAY faster
+import pyautogui          # To locate pixels in the screen
+import time               # To be able to give delay between actions
+import keyboard           # To be able to Use the keyboard actions
+import random             # To give random values, give a more human apearance for certain actions that requiere
+import win32api, win32con # To be able to use the Mouse events, a windows API, faster than the Pythion API
+# Could use pyautogui to click, but as said, Windows API is WAY faster
 
-//Defines a "procedure" or a "function" to be called, receiving X and Y
+#Defines a "procedure" or a "function" to be called, receiving X and Y
 def click(x,y):
-    win32api.SetCursorPos((x,y))                            //set the cursor at X, Y coordinates
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) // defines a event to happen, pressing the left button
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)   // defines a event to happen, un pressing the left button
+    win32api.SetCursorPos((x,y))                            #set the cursor at X, Y coordinates
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) # defines a event to happen, pressing the left button
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)   # defines a event to happen, unpressing the left button
+
+time.sleep(0.1) # delay of 0,1s for the next action
+
+# If coordinate in screen is black (0) click on the same position
+#  command       coordinate - color(first spectrum of "XXX, XXX, XXX") is equal to 0 (black)
+if pyautogui.pixel(581, 400)  [0] == 0:
+    click(581, 400)
+
+#============================================================================================================
+#displays the mouse position
+
+import pyautogui
+pyautogui.displayMousePosition()
+
+
